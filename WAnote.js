8065719404ed650f6cd64db21202425b34aec4b5
r14minji <rkdalswl0728@gmail.com>
2021-05-10 23:12:08 +0900

    test

--- WAnote.js
+++ WAnote.js
@@ -1,89 +1,104 @@
-
 // sample1의 card_banner 만들고 싶어서
 
- const cards = ['http://img.just-one.co.kr/shopping_mall/mainimage/banner_four1.jpg','http://img.just-one.co.kr/shopping_mall/mainimage/banner_four2.jpg','http://img.just-one.co.kr/shopping_mall/mainimage/banner_four3.gif','http://img.just-one.co.kr/shopping_mall/mainimage/banner_four4.jpg']
-    cards.map((link)=> {
-        const img = document.createElement('IMG');
-        const card = document.getElementById('card');
-        const banner = document.querySelector('.card_banner');
-        banner.appendChild(card);
-        card.appendChild(img);
-        img.src = link
-    })
+const cards = [
+  "http://img.just-one.co.kr/shopping_mall/mainimage/banner_four1.jpg",
+  "http://img.just-one.co.kr/shopping_mall/mainimage/banner_four2.jpg",
+  "http://img.just-one.co.kr/shopping_mall/mainimage/banner_four3.gif",
+  "http://img.just-one.co.kr/shopping_mall/mainimage/banner_four4.jpg",
+];
+cards.map((link) => {
+  const img = document.createElement("IMG");
+  const card = document.getElementById("card");
+  const banner = document.querySelector(".card_banner");
+  banner.appendChild(card);
+  card.appendChild(img);
+  img.src = link;
+});
 
-//A: 요소가 틀렸음. createElement('요소') 
+//A: 요소가 틀렸음. createElement('요소')
 //요소에 대한 설명은 https://heropy.blog/2019/05/26/html-elements/
-
 
 //sample2의 window_banner 만들고 싶어서
 //카드배너꺼 가지고왔씀 위와 아래 비교해서 수정함.
-const links = ['http://img.just-one.co.kr/shopping_mall/mainimage/banner_four1.jpg','http://img.just-one.co.kr/shopping_mall/mainimage/banner_four2.jpg','http://img.just-one.co.kr/shopping_mall/mainimage/banner_four3.gif','http://img.just-one.co.kr/shopping_mall/mainimage/banner_four4.jpg']
-    links.map((link) => {
-        const div = document.createElement("DIV");
-        const img = document.createElement('IMG');
-        const cardbanner = document.querySelector('.card_banner')
-        div.className = ('card');
-        img.src = link;
-        
-        cardbanner.appendChild(div);
-        div.appendChild(img);
+const links = [
+  "http://img.just-one.co.kr/shopping_mall/mainimage/banner_four1.jpg",
+  "http://img.just-one.co.kr/shopping_mall/mainimage/banner_four2.jpg",
+  "http://img.just-one.co.kr/shopping_mall/mainimage/banner_four3.gif",
+  "http://img.just-one.co.kr/shopping_mall/mainimage/banner_four4.jpg",
+];
+links.map((link) => {
+  const div = document.createElement("DIV");
+  const img = document.createElement("IMG");
+  const cardbanner = document.querySelector(".card_banner");
+  div.className = "card";
+  img.src = link;
 
-    })
-    
-    const BWdiv = document.createElement('DIV');
-    const BWwarp = document.querySelector('.banner_wrap');
-    BWdiv.className = 'banner'
-    const BWlinks = ['https://image.adidas.co.kr/upload/banner/174df423138247e689a80e1d7356f65d_0205165958.JPG', 'https://image.adidas.co.kr/upload/banner/588ec794838f4252ab424f29e7c80d9d_0226134623.jpg', 'https://image.adidas.co.kr/upload/banner/ec600721ca254075be354a5400160b7b_0219105556.jpg','https://image.adidas.co.kr/upload/banner/12680e1c5ad64bbd8cffaa4f73298c94_0303110225.jpg']
-    BWlinks.map((BWlink) => {
-        const BWimg = document.createElement('IMG');
-        BWimg.src = "BWlink";
-    })
-    BWdiv.appendChild('BWlink')
+  cardbanner.appendChild(div);
+  div.appendChild(img);
+});
 
-    const BWbuttons = ['구매하기', '구매하기', '자세히보기', '자세히보기'];
-    BWbuttons.map((BWbutton) => {
-        const BW_button = document.createElement('BUTTON');
-        BW_button.innerText = 'BWbutton'; 
-    })
-    BWdiv.appendChild('BWbuttons')
-    BWwarp.appendChild('BWdiv');
+const BWdiv = document.createElement("DIV");
+const BWwarp = document.querySelector(".banner_wrap");
+BWdiv.className = "banner";
+const BWlinks = [
+  "https://image.adidas.co.kr/upload/banner/174df423138247e689a80e1d7356f65d_0205165958.JPG",
+  "https://image.adidas.co.kr/upload/banner/588ec794838f4252ab424f29e7c80d9d_0226134623.jpg",
+  "https://image.adidas.co.kr/upload/banner/ec600721ca254075be354a5400160b7b_0219105556.jpg",
+  "https://image.adidas.co.kr/upload/banner/12680e1c5ad64bbd8cffaa4f73298c94_0303110225.jpg",
+];
+BWlinks.map((BWlink) => {
+  const BWimg = document.createElement("IMG");
+  BWimg.src = "BWlink";
+});
+BWdiv.appendChild("BWlink");
 
+const BWbuttons = ["구매하기", "구매하기", "자세히보기", "자세히보기"];
+BWbuttons.map((BWbutton) => {
+  const BW_button = document.createElement("BUTTON");
+  BW_button.innerText = "BWbutton";
+});
+BWdiv.appendChild("BWbuttons");
+BWwarp.appendChild("BWdiv");
 
-    //sample2의 banner_wrap 만들 때, 이중으로 map함수를 사용할 경우 >>3번쨰 오답노트 이미지파일 참고
-    const BWlinks = ['https://image.adidas.co.kr/upload/banner/174df423138247e689a80e1d7356f65d_0205165958.JPG', 'https://image.adidas.co.kr/upload/banner/588ec794838f4252ab424f29e7c80d9d_0226134623.jpg', 'https://image.adidas.co.kr/upload/banner/ec600721ca254075be354a5400160b7b_0219105556.jpg','https://image.adidas.co.kr/upload/banner/12680e1c5ad64bbd8cffaa4f73298c94_0303110225.jpg']
-    const BWbuttons = ['구매하기', '구매하기', '자세히보기', '자세히보기'];
-    BWlinks.map((BWlink) => {
-        const BWdiv = document.createElement('DIV');
-        const BWimg = document.createElement('IMG');
-        const BWwarp = document.querySelector('.banner_wrap');
-        BWdiv.className = 'banner'
-        BWimg.src = BWlink;
-        BWdiv.appendChild(BWimg);
+//sample2의 banner_wrap 만들 때, 이중으로 map함수를 사용할 경우 >>3번쨰 오답노트 이미지파일 참고
+const BWlinks = [
+  "https://image.adidas.co.kr/upload/banner/174df423138247e689a80e1d7356f65d_0205165958.JPG",
+  "https://image.adidas.co.kr/upload/banner/588ec794838f4252ab424f29e7c80d9d_0226134623.jpg",
+  "https://image.adidas.co.kr/upload/banner/ec600721ca254075be354a5400160b7b_0219105556.jpg",
+  "https://image.adidas.co.kr/upload/banner/12680e1c5ad64bbd8cffaa4f73298c94_0303110225.jpg",
+];
+const BWbuttons = ["구매하기", "구매하기", "자세히보기", "자세히보기"];
+BWlinks.map((BWlink) => {
+  const BWdiv = document.createElement("DIV");
+  const BWimg = document.createElement("IMG");
+  const BWwarp = document.querySelector(".banner_wrap");
+  BWdiv.className = "banner";
+  BWimg.src = BWlink;
+  BWdiv.appendChild(BWimg);
 
-        BWbuttons.map((BWbutton) => {
-            const Newbutton = document.createElement('BUTTON');
-            Newbutton.innerText = BWbutton
-            BWdiv.appendChild(Newbutton); // BWdiv를 가지고 올 수 있는건지?
-        })
+  BWbuttons.map((BWbutton) => {
+    const Newbutton = document.createElement("BUTTON");
+    Newbutton.innerText = BWbutton;
+    BWdiv.appendChild(Newbutton); // BWdiv를 가지고 올 수 있는건지?
+  });
 
-        BWwarp.appendChild(BWdiv);
-    })
+  BWwarp.appendChild(BWdiv);
+});
 
+//유튜브. 이게 정답/ li태그 연결할 때, 자손관계 헷갈리면 안나온다아.
+MenuList.map((Mlist) => {
+  const MenuGroup = document.querySelector(".menu_groub ul");
+  const MenuContent = document.createElement("li");
+  const MenuLine = document.createElement("p");
+  const MenuFavicon = document.createElement("i");
+  const MenuName = document.createElement("p");
 
+  MenuFavicon.className = Mlist.MenuFA;
+  MenuName.innerText = Mlist.MunuTITLE;
+  MenuGroup.appendChild(MenuContent);
+  MenuContent.appendChild(MenuLine);
+  MenuContent.appendChild(MenuName);
+  MenuLine.appendChild(MenuFavicon); //html에 p와 li를 나란히 썼으나 자손관계다! 이거 잘 못 연결하면 오류!
+});
 
-    //유튜브. 이게 정답/ li태그 연결할 때, 자손관계 헷갈리면 안나온다아.
-    MenuList.map((Mlist) => {
-        const MenuGroup = document.querySelector('.menu_groub ul');
-        const MenuContent = document.createElement('li');
-        const MenuLine = document.createElement('p');
-        const MenuFavicon = document.createElement('i');
-        const MenuName = document.createElement('p');
-
-        MenuFavicon.className = Mlist.MenuFA;
-        MenuName.innerText = Mlist.MunuTITLE;
-        MenuGroup.appendChild(MenuContent);
-        MenuContent.appendChild(MenuLine);
-        MenuContent.appendChild(MenuName);
-        MenuLine.appendChild(MenuFavicon); //html에 p와 li를 나란히 썼으나 자손관계다! 이거 잘 못 연결하면 오류!
-
-    })
+//test
